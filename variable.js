73f63022fd58896a261abf4e0832b28513f77318
palsana vivek <vickypalsana24@gmail.com>
2026-01-20 09:28:44 +0000

    config done

---
+++ variable.js
@@ -0,0 +1,10 @@
+const accountId = 241204
+let accountName = 'Acme Corporation'
+var accountType = 'saving'
+
+// accountId = 24120425545
+accountName = 'aasvek Pvt ltd'
+accountType = 'current'
+
+console.log(accountId);
+console.table([accountId, accountName, accountType]);

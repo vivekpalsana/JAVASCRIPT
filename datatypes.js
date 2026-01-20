7bf3593e8e7e9df96ebabc5aeaf7f90f471bb837
palsana vivek <vickypalsana24@gmail.com>
2026-01-20 10:36:52 +0000

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,14 @@
+let name = "vicky"
+let age = 18
+let state
+
+console.log(name);
+console.log(typeof name)
+
+
+// null
+// Number
+// BigInt
+// undefined          // this all are datatypes are used in js.
+// Boolean
+// String
